1fe512703f41f7a40f063ad6b8370bf3ce03a966
othienoJoe <bryo094@gmail.com>
2021-10-17 18:56:27 +0300

    updated the register section,urls and views files

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-	path('register', views.RegistrationView.as_view(), name='register')
+	path('register', views.RegistrationView.as_view(), name='register'),
+	path('login', views.LoginView.as_view(), name='login')
 ]
--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views.generic import View
 from validate_email import validate_email
 from django.contrib import messages
+from django.contrib.auth.models import User
 
 # Create your views here.
 class RegistrationView(View):
@@ -9,15 +10,51 @@ class RegistrationView(View):
 		return render(request, 'auth/register.html')
 
 	def post(self, request):
-		data = request.POST
+		context={
+			'data': request.POST,
+			'has_error': False
+		}
 		email = request.POST.get('email')
+		username = request.POST.get('username')
+		full_name = request.POST.get('name')
 		password = request.POST.get('password')
 		password2 = request.POST.get('password2')
 
 		if not validate_email(email):
 			messages.add_message(request, messages.ERROR, 'Please Enter A Valid Email!')
+			context['has_error'] = True
 
 		if not validate_email(email):
 			messages.add_message(request, messages.ERROR, 'Passwords MUST contain at least 6 characters!')
+			context['has_error'] = True
 
-		return render(request, 'auth/register.html', context={'data': data})
+		if password != password2:
+			messages.add_message(request, messages.ERROR, 'Passwords DO NOT Match !')
+			context['has_error'] = True
+
+		if User.objects.filter(email = email).exists():
+			messages.add_message(request, messages.ERROR, 'Email Already Taken !')
+			context['has_error'] = True
+
+		if User.objects.filter(username = username).exists():
+			messages.add_message(request, messages.ERROR, 'Username Already Taken !')
+			context['has_error'] = True
+
+		if context['has_error']:
+		  return render(request, 'auth/register.html', context)
+
+		user = User.objects.create_user(username = username, email = email)
+		user.set_password(password)
+		user.first_name = full_name
+		user.last_name = full_name
+		user.is_active = False
+
+		user.save()
+
+		messages.add_message(request, messages.SUCCESS, 'Account Successfully Created !')
+
+		return redirect('login')
+
+class LoginView(View):
+	def get(self, request):
+		return render(request, 'auth/login.html')
