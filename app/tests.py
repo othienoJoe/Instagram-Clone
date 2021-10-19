132b90ae882f54e3738c8e89de6cd0aaeb9b685c
othienoJoe <bryo094@gmail.com>
2021-10-19 14:22:56 +0300

    all the tests done and ran successfully

--- app/tests.py
+++ app/tests.py
@@ -1,5 +1,9 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.contrib.auth.models import User
+from django.utils.encoding import force_bytes,force_text,DjangoUnicodeDecodeError
+from django.utils.http import urlsafe_base64_encode,urlsafe_base64_decode
+from .utils import generate_token
 
 # Create your tests here.
 class BaseTest(TestCase):
@@ -65,3 +69,56 @@ class RegisterTest(BaseTest):
 			self.client.post(self.register_url,self.user,format='text/html')
 			response=self.client.post(self.register_url,self.user,format='text/html')
 			self.assertEqual(response.status_code,400)
+
+
+class LoginTest(BaseTest):
+	def test_can_access_page(self):
+		response=self.client.get(self.login_url)
+		self.assertEqual(response.status_code,200)
+		self.assertTemplateUsed(response,'auth/login.html')
+
+	def test_login_success(self):
+		self.client.post(self.register_url,self.user,format='text/html')
+		user=User.objects.filter(email=self.user['email']).first()
+		user.is_active=True
+		user.save()
+		response= self.client.post(self.login_url,self.user,format='text/html')
+		self.assertEqual(response.status_code,302)
+
+	def test_cantlogin_with_unverified_email(self):
+		self.client.post(self.register_url,self.user,format='text/html')
+		response= self.client.post(self.login_url,self.user,format='text/html')
+		self.assertEqual(response.status_code,401)
+
+	def test_cantlogin_with_no_username(self):
+		response= self.client.post(self.login_url,{'password':'passwped','username':''},format='text/html')
+		self.assertEqual(response.status_code,401)
+
+	def test_cantlogin_with_no_password(self):
+		response= self.client.post(self.login_url,{'username':'passwped','password':''},format='text/html')
+		self.assertEqual(response.status_code,401)
+
+class UserVerifyTest(BaseTest):
+    def test_user_ctivates_success(self):
+        user=User.objects.create_user('testuser','crytest@gmail.com')
+        user.set_password('tetetebvghhhhj')
+        user.is_active=False
+        user.save()
+        uid=urlsafe_base64_encode(force_bytes(user.pk))
+        token=generate_token.make_token(user)
+        response=self.client.get(reverse('activate',kwargs={'uidb64':uid,'token':token}))
+        self.assertEqual(response.status_code,302)
+        user=User.objects.get(email='crytest@gmail.com')
+        self.assertTrue(user.is_active)
+
+    def test_user_cant_ctivates_succesfully(self):
+        user=User.objects.create_user('testuser','crytest@gmail.com')
+        user.set_password('tetetebvghhhhj')
+        user.is_active=False
+        user.save()
+        uid=urlsafe_base64_encode(force_bytes(user.pk))
+        token=generate_token.make_token(user)
+        response=self.client.get(reverse('activate',kwargs={'uidb64':'uid','token':'token'}))
+        self.assertEqual(response.status_code,401)
+        user=User.objects.get(email='crytest@gmail.com')
+        self.assertFalse(user.is_active)
