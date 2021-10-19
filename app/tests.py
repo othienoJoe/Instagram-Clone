e73f54f5262ae5390a3a9db90631d278182c2620
othienoJoe <bryo094@gmail.com>
2021-10-19 12:15:27 +0300

    test for user and password length ran successfully

--- app/tests.py
+++ app/tests.py
@@ -14,8 +14,24 @@ class BaseTest(TestCase):
 			'name':'fullname'
 		}
 
+		self.user_short_password={
+			'email':'testemail@gmail.com',
+			'username':'username',
+			'password':'tes',
+			'password2':'tes',
+			'name':'fullname'
+    }
+
 class RegisterTest(BaseTest):
   def test_can_view_page_correctly(self):
 			response=self.client.get(self.register_url)
 			self.assertEqual(response.status_code,200)
 			self.assertTemplateUsed(response,'auth/register.html')
+
+  def test_can_register_user(self):
+			response=self.client.post(self.register_url,self.user,format='text/html')
+			self.assertEqual(response.status_code,302)
+
+  def test_cant_register_user_withshortpassword(self):
+			response=self.client.post(self.register_url,self.user_short_password,format='text/html')
+			self.assertEqual(response.status_code,400)
